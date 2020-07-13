b0cf13f2a755ff9e447934b80d882d46c1fd4dba
s4rvesh <sarvesh.upadhye@gmail.com>
2020-07-13 10:49:01 +0530

    Json correction

--- handler/createUser.go
+++ handler/createUser.go
@@ -9,9 +9,9 @@ import (
 )
 
 type Users struct {
-	name     string `json:"name"`
-	email    string `json:"email"`
-	password string `json:"password"`
+	Name     string `json:"Name"`
+	Email    string `json:"Email"`
+	Password string `json:"Password"`
 }
 
 //CreateUser adds a new user to system
@@ -21,24 +21,32 @@ func CreateUser(c *gin.Context) {
 
 	user := Users{}
 
-	c.Bind(&user)
+	err := c.Bind(&user)
 
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	fmt.Println(user.Name)
+
+	fmt.Println(user.Email)
+	fmt.Println(user.Password)
 	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/twitter")
 
 	if err != nil {
 		panic(err.Error())
 	}
 
-	_, errQ := db.Query("INSERT INTO users(uid, name, email, password) VALUES (?,?,?,?)", user.name, user.email, user.password)
+	_, errQ := db.Query("INSERT INTO users(name, email, password) VALUES (?,?,?)", user.Name, user.Email, user.Password)
 
 	if errQ != nil {
 		panic(err.Error())
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"Name":     user.name,
-		"Email":    user.name,
-		"Password": user.password,
+		"Name":     user.Name,
+		"Email":    user.Email,
+		"Password": user.Password,
 	})
 
 	defer db.Close()
